ee0b09eb288e691f3e98550378d6d8d406b32c9d
Ali Shah <s.ashah@outlook.com>
2021-10-15 11:41:27 -0400

    [Added] date formatting filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
+const { DateTime } = require('luxon');
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('./src/style.css');
+
+  eleventyConfig.addShortcode('year', () => `${new Date().getUTCFullYear()}`);
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
   return {
     dir: { input: 'src' }
   }
